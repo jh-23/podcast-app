e78d01e0248472a300f1df5260395f2bf86166c4
John Heger <heger7@gmail.com>
2024-05-11 15:49:29 -0500

    added serialize_rules

--- server/models.py
+++ server/models.py
@@ -8,6 +8,8 @@ from config import db
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
+    serialize_rules = ('-user_podcast_reviews.user', )
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     password = db.Column(db.String)
@@ -19,17 +21,24 @@ class User(db.Model, SerializerMixin):
 class UserPodcastReview(db.Model, SerializerMixin):
     __tablename__ = 'user_podcast_reviews'
     
+    serialize_rules = ('-user.user_podcast_reviews', '-podcast.user_podcast_reviews')
+    
     id = db.Column(db.Integer, primary_key=True)
     
-    user = db.relationship('User', backpopulates=(''))
+    #relationship
+    user = db.relationship('User', backpopulates=('user_podcast_reviews'))
     
     #Foreign Keys
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     podcast_id = db.Column(db.Integer, db.ForeignKey('podcasts.id'))
 
+    #relationship
+    podcast = db.relationship('Podcast', backpopulates=('user_podcast_review'))
 
 class Podcast(db.Model, SerializerMixin):
     __tablename__ = 'podcasts'
+    
+    serialize_rules = ('-user_podcast_reviews.podcast', )
     
     id = db.Column(db.Integer, primary_key=True)
     channel = db.Column(db.String)
@@ -37,6 +46,8 @@ class Podcast(db.Model, SerializerMixin):
     episodes = db.Column(db.String)
     image = db.Column(db.String)
 
+    #relationship
+    user_podcast_reviews = db.relationship('UserPodcastReview', backpopulates=('podcast'))
     
     
     
