d7f200f1ecd750b57ff005c2d04631a94f4fa639
John Heger <heger7@gmail.com>
2024-05-11 11:40:46 -0500

    added User model

--- server/config.py
+++ server/config.py
@@ -28,4 +28,4 @@ db.init_app(app)
 api = Api(app)
 
 # Instantiate CORS
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
--- server/models.py
+++ server/models.py
@@ -4,3 +4,25 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __table__name = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    
+    
+class UserPodcastReviews(db.Model, SerializerMixin):
+    pass
+
+
+
+class Podcast(db.Model, SerializerMixin):
+    __table__name = 'podcasts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column()
+    
+    
+
