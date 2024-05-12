cd3b7ea5f9b3c471726ca0eb74523726279fb03f
John Heger <heger7@gmail.com>
2024-05-12 13:40:20 -0500

    added GET users route

--- server/app.py
+++ server/app.py
@@ -3,19 +3,32 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-from models import User, Podcast, UserPodcastReview
+from models import db, User, Podcast, UserPodcastReview
 
 # Views go here!
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+@app.route('/users', methods=['GET'])
+def users():
+    
+    users = [user.to_dict() for user in User.query.all()]
+    
+    response = make_response(
+        users,
+        200
+    )
+    
+    return response
+    
 
 
 if __name__ == '__main__':
